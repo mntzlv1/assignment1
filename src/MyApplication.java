60056ffdf7de820bea363d3fb3381e46c60195ba
Amina <231599@astanait.edu.kz>
2023-12-25 19:40:44 +0600

    First init

--- src/MyApplication.java
+++ src/MyApplication.java
@@ -1,17 +1,33 @@
+import java.util.Scanner;
+
 public class MyApplication {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        Point p1 = new Point(2, 0);
-        Point p2 = new Point(3, 4);
-        Point p3 = new Point(7, 5);
-        Point p4 = new Point(0, 6);
-        Point p5 = new Point(8, 1);
-        Point p6 = new Point(1, 2);
-        Point[] points = {p1, p2,p3,p4,p5,p6};
+        // in assignment shape consists 6 points so u should input it
+        System.out.print("number of points: ");
+        int numPoints = scanner.nextInt();
+
+        //  array in order to store the points
+        Point[] points = new Point[numPoints];
+
+        for (int i = 0; i < numPoints; i++) {
+            System.out.print(" x-coordinate" + (i + 1) + ": ");
+            double x = scanner.nextDouble();
+
+            System.out.print(" y-coordinate" + (i + 1) + ": ");
+            double y = scanner.nextDouble();
+
+            points[i] = new Point(x, y);
+        }
+
+        // it is a shape  with the input points
         Shape shape = new Shape(points);
 
         System.out.println("Perimeter: " + shape.perimeter());
         System.out.println("Longest Side: " + shape.longestSide());
         System.out.println("Average Side: " + shape.averageSide());
+
+        scanner.close();
     }
 }
